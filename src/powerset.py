aae9400948cbdafe60bff0c725ee499bdfd4528b
currocam <campuzanocurro@gmail.com>
2022-09-13 17:13:36 +0200

    feat: test are passing

--- src/powerset.py
+++ src/powerset.py
@@ -8,5 +8,19 @@ def power(x: list[Any]) -> list[list[Any]]:
     Compute the power-set of x.
 
     Returns the power-set of x as a list of lists.
+
+    >>> power([])
+    [[]]
+    >>> power([1])
+    [[], [1]]
+    >>> power([1, 2])
+    [[], [2], [1], [1, 2]]
     """
-    return []  # FIXME: you need to add some code here.
+    n = len(x)
+    numbers = list(range(2**n))
+    bits = [format(i, "b").zfill(len(x)) for i in numbers]
+    power_set = list()
+    for index in bits:
+        subset = [x[i] for i, v in enumerate(index) if v == '1']
+        power_set.append(subset)
+    return power_set
